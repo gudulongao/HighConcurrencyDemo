6603dedaea3eeff94df28846dba16d324ee4632d
gudulongao <gudulongao@qq.com>
2018-01-10 23:14:59 +0800

    修改重入锁注释

--- src/main/demo/concurrency/reentranlock/ReentranLockDemo.java
+++ src/main/demo/concurrency/reentranlock/ReentranLockDemo.java
@@ -2,17 +2,22 @@ package demo.concurrency.reentranlock;
 
 import java.util.concurrent.locks.ReentrantLock;
 
+/**
+ * ReetranLock 样例
+ */
 public class ReentranLockDemo implements Runnable {
     public static ReentrantLock lock = new ReentrantLock();
     public static int i = 0;
 
     @Override
     public void run() {
-        for (int j = 0; i < 100000; j++) {
+        for (int j = 0; j < 10000; j++) {
+            //手动加锁
             lock.lock();
             try {
                 i++;
             } finally {
+                //必须手动释放锁
                 lock.unlock();
             }
         }
